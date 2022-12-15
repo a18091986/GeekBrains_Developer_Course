a1e2b51148f9536be47de47f43d4302512fa4674
behappyman <behappyman@mail.ru>
2022-12-15 18:58:01 +0300

    HW_5

--- 1_C#/Practice/5_Seminar.cs
+++ 1_C#/Practice/5_Seminar.cs
@@ -1,54 +1,54 @@
 // -----------------------task_1---------------------------------
 // Просуммировать все отрицательные элементы массива
 
-int[] GenerateRandomArray(int arr_size, int min_val, int max_val) {
-    int[] array = new int[arr_size];
+// int[] GenerateRandomArray(int arr_size, int min_val, int max_val) {
+//     int[] array = new int[arr_size];
 
-    for (int i = 0; i < arr_size; i++)
-        array[i] = new Random().Next(min_val, max_val + 1);
+//     for (int i = 0; i < arr_size; i++)
+//         array[i] = new Random().Next(min_val, max_val + 1);
     
-    return array;
-}
+//     return array;
+// }
 
-void PrintArray(int[] arr) {
-    for (int i = 0; i < arr.Length; i++)
-        Console.Write($"{arr[i]} ");
-}
+// void PrintArray(int[] arr) {
+//     for (int i = 0; i < arr.Length; i++)
+//         Console.Write($"{arr[i]} ");
+// }
 
 
-int FindSumNegativeElements(int[] arr) {
-    int sum = 0;
-    for (int i = 0; i < arr.Length; i++)
-        if (arr[i] < 0)
-            sum += arr[i];
+// int FindSumNegativeElements(int[] arr) {
+//     int sum = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//         if (arr[i] < 0)
+//             sum += arr[i];
 
-    return sum;
-}
+//     return sum;
+// }
 
-int[] ReplacePositiveAndNegative(int[] arr) {
-    for (int i = 0; i < arr.Length; i++)
-        arr[i] *= -1;
-    return arr;
-}
+// int[] ReplacePositiveAndNegative(int[] arr) {
+//     for (int i = 0; i < arr.Length; i++)
+//         arr[i] *= -1;
+//     return arr;
+// }
 
-bool IsElementInArray(int[] arr, int element_to_find) {
-    bool result = false;
-    for (int i = 0; i < arr.Length; i++)
-        if (arr[i] == element_to_find) {
-            result = true;
-            break;
-        }
+// bool IsElementInArray(int[] arr, int element_to_find) {
+//     bool result = false;
+//     for (int i = 0; i < arr.Length; i++)
+//         if (arr[i] == element_to_find) {
+//             result = true;
+//             break;
+//         }
             
-    return result; 
-}
+//     return result; 
+// }
 
-int CountElementsOfMassiveInRange(int[] arr, int min_val, int max_val) {
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
-        if (arr[i] >= min_val && arr[i] <= max_val)
-            count += 1;
-    return count;
-}
+// int CountElementsOfMassiveInRange(int[] arr, int min_val, int max_val) {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//         if (arr[i] >= min_val && arr[i] <= max_val)
+//             count += 1;
+//     return count;
+// }
 
 // int[] arr = GenerateRandomArray(arr_size: 10, min_val: -1, max_val: 10);
 // PrintArray(arr);
@@ -81,7 +81,7 @@ int CountElementsOfMassiveInRange(int[] arr, int min_val, int max_val) {
 // Задайте массив из m случайных чисел. Найдите количество элементов массива, 
 // значения которых лежат в отрезке [a,b].
 
-int[] arr = GenerateRandomArray(10, -10, 10);
-PrintArray(arr);
-Console.WriteLine();
-Console.WriteLine(CountElementsOfMassiveInRange(arr, -10, 10));
+// int[] arr = GenerateRandomArray(10, -10, 10);
+// PrintArray(arr);
+// Console.WriteLine();
+// Console.WriteLine(CountElementsOfMassiveInRange(arr, -10, 10));
---
+++ 1_C#/Practice/HW_5.cs
@@ -0,0 +1,85 @@
+int[] GenerateRandomIntArray(int arr_size, int min_val, int max_val) {
+    int[] array = new int[arr_size];
+
+    for (int i = 0; i < arr_size; i++)
+        array[i] = new Random().Next(min_val, max_val + 1);
+    
+    return array;
+}
+
+double[] GenerateRandomDoubleArray(int arr_size, double min_val, double max_val) {
+    double [] array = new double[arr_size];
+
+    for (int i = 0; i < arr_size; i++)
+        array[i] = new Random().NextDouble() * (max_val - min_val) + min_val;
+    
+    return array;
+}
+
+void PrintIntArray(int[] arr) {
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+}
+
+void PrintDoubleArray(double[] arr) {
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{Math.Round(arr[i], 2)} ");
+}
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// int CountEvenNumbersInArray(int[] arr) {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//         if (arr[i] % 2 == 0)
+//             count++;
+
+//     return count;
+// }
+
+// int[] arr = GenerateRandomIntArray(arr_size: 10, min_val: 100, max_val: 999);
+// PrintIntArray(arr);
+// Console.WriteLine();
+// Console.WriteLine($"Count of even numbers in array: {CountEvenNumbersInArray(arr)}");
+
+
+// [345, 897, 568, 234] -> 2
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// int SumOddIndexNumbersInArray(int[] arr) {
+//     int sum = 0;
+//     for (int i = 1; i < arr.Length; i+=2)
+//         sum+=arr[i];
+
+//     return sum;
+// }
+
+// int[] arr = GenerateRandomIntArray(arr_size: 10, min_val: 1, max_val: 10);
+// PrintIntArray(arr);
+// Console.WriteLine();
+// Console.WriteLine($"Sum of numbers on odd position in array: {SumOddIndexNumbersInArray(arr)}");
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// double DifferenceBetweenMaxAndMin(double[] arr) {
+//     double min = arr[0];
+//     double max = arr[0];
+//     for (int i = 1; i < arr.Length; i++) {
+//         if (arr[i] < min)
+//             min = arr[i];
+//         if (arr[i] > max)
+//             max = arr[i];
+//     }
+//     return max - min;
+// }
+
+// double[] arr = GenerateRandomDoubleArray(arr_size: 10, min_val: 1.0, max_val: 10.0);
+// PrintDoubleArray(arr);
+// Console.WriteLine();
+// Console.WriteLine($"Difference between max and min elements of array: {Math.Round(DifferenceBetweenMaxAndMin(arr), 2)}");
+
+// [3 7 22 2 78] -> 76
