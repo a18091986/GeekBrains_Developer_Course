2764e3f5420b68d154f516d0dd7b0c7151891cda
Andrey <a18091986@gmail.com>
2022-12-26 14:54:30 +0300

    C# ready

--- 1_C#/Practice/9_Seminar.cs
+++ 1_C#/Practice/9_Seminar.cs
@@ -1,8 +1,23 @@
 // ---------------------------------------------Task_1-----------------------------------------------
-// 
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
 
+// void DigitsRangePrint(int N) {
+//     if (N > 1) DigitsRangePrint(N - 1);
+//     Console.Write($"{N} ");
+// }
 
+// void DigitsRangePrintReverse(int N) {
+//     Console.Write($"{N} ");
+//     if (N > 1) {
+//         DigitsRangePrintReverse(N - 1);
+//     }
+// }
 
+// Console.Write("Input N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// DigitsRangePrint(N);
+// Console.WriteLine();
+// DigitsRangePrintReverse(N);
 
 // ---------------------------------------------Task_2-----------------------------------------------
 // Найдите сумму цифр числа
@@ -20,6 +35,23 @@
 // ---------------------------------------------Task_3-----------------------------------------------
 // Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 
+void DigitsRangePrintMN(int M, int N) {
+    int min = (M < N) ? M : N;
+    int max = (M < N) ? N : M;
+    if (min < max) {
+        Console.Write($"{min} ");
+        DigitsRangePrintMN(min + 1, max);
+    } else {
+        Console.Write($"{max} ");
+    }
+}
+
+// Console.Write("Input M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// DigitsRangePrintMN(M, N);
 
 
 
@@ -28,6 +60,22 @@
 // Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
 
 
+double ApowB(int a, int b) {
+
+    if (b >= 1) 
+        return a * ApowB(a, b - 1);
+    else if (b <= -1)
+        return (1.0 / a) * ApowB(a, b + 1);
+    else
+        return 1;
+}
+
+Console.Write("Input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ApowB(a, b));
 
 
 
--- 1_C#/Practice/HW_9.cs
+++ 1_C#/Practice/HW_9.cs
@@ -1,60 +1,60 @@
-// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-void PrintDigitsrecursive(int N) {
-    if (N >= 1) {
-        Console.Write($"{N} ");
-        PrintDigitsrecursive(N-1);
-    }
-}
+// void PrintDigitsrecursive(int N) {
+//     if (N >= 1) {
+//         Console.Write($"{N} ");
+//         PrintDigitsrecursive(N-1);
+//     }
+// }
 
-// Console.WriteLine("Input N: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-// PrintDigitsrecursive(N);
+// // Console.WriteLine("Input N: ");
+// // int N = Convert.ToInt32(Console.ReadLine());
+// // PrintDigitsrecursive(N);
 
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// // N = 5 -> "5, 4, 3, 2, 1"
+// // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-int RecursiveSum(int N, int M) {
-    int min = (M <= N) ? M : N;
-    int max = (M <= N) ? N : M;
-    if (min < max) {
-        return min + RecursiveSum(min+1, max);
-    } else {
-        return max;
-    }
-}
+// int RecursiveSum(int N, int M) {
+//     int min = (M <= N) ? M : N;
+//     int max = (M <= N) ? N : M;
+//     if (min < max) {
+//         return min + RecursiveSum(min+1, max);
+//     } else {
+//         return max;
+//     }
+// }
 
-// Console.Write("Input N: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input M: ");
-// int M = Convert.ToInt32(Console.ReadLine());
-// int sum = RecursiveSum(M, N);
-// Console.WriteLine($"Sum: {sum}");
+// // Console.Write("Input N: ");
+// // int N = Convert.ToInt32(Console.ReadLine());
+// // Console.Write("Input M: ");
+// // int M = Convert.ToInt32(Console.ReadLine());
+// // int sum = RecursiveSum(M, N);
+// // Console.WriteLine($"Sum: {sum}");
 
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
 
-// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-int Accerman(int m, int n) {
-    if (m == 0)
-        return n + 1;
-    else {
-        if ((m != 0) && (n == 0))
-            return Accerman(m - 1, 1);
-    else
-      return Accerman(m - 1, Accerman(m, n - 1));
-    }
-}
+// int Accerman(int m, int n) {
+//     if (m == 0)
+//         return n + 1;
+//     else {
+//         if ((m != 0) && (n == 0))
+//             return Accerman(m - 1, 1);
+//     else
+//       return Accerman(m - 1, Accerman(m, n - 1));
+//     }
+// }
 
-Console.Write("Input m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int accerman = Accerman(m, n);
-Console.WriteLine($"Result: {accerman}");
+// Console.Write("Input m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int accerman = Accerman(m, n);
+// Console.WriteLine($"Result: {accerman}");
 
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
+// // m = 2, n = 3 -> A(m,n) = 9
+// // m = 3, n = 2 -> A(m,n) = 29
